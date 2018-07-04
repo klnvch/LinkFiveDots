0be54c1a8fc20b4b8ca323407f250b823f96e356
klnvch <klnvch@gmail.com>
2018-07-04 12:02:01 +0300

    4.4.6

--- app/src/main/java/by/klnvch/link5dots/utils/ActivityUtils.java
+++ app/src/main/java/by/klnvch/link5dots/utils/ActivityUtils.java
@@ -6,19 +6,25 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 
+import com.crashlytics.android.Crashlytics;
+
 import static com.google.android.gms.common.internal.Preconditions.checkNotNull;
 
 public class ActivityUtils {
     public static void showDialog(@NonNull FragmentManager manager,
-                            @NonNull DialogFragment dialog,
-                            @NonNull String tag) {
+                                  @NonNull DialogFragment dialog,
+                                  @NonNull String tag) {
         checkNotNull(manager);
         checkNotNull(dialog);
         checkNotNull(tag);
 
-        final FragmentTransaction ft = manager.beginTransaction();
-        final Fragment dialogPrevious = manager.findFragmentByTag(tag);
-        if (dialogPrevious != null) ft.remove(dialogPrevious);
-        dialog.show(ft, tag);
+        try {
+            final FragmentTransaction ft = manager.beginTransaction();
+            final Fragment dialogPrevious = manager.findFragmentByTag(tag);
+            if (dialogPrevious != null) ft.remove(dialogPrevious);
+            dialog.show(ft, tag);
+        } catch (Exception e) {
+            Crashlytics.logException(e);
+        }
     }
 }
